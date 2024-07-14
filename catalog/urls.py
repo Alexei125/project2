20e8f7923d3006e609906fa51e400448e612f08f
Alexei <rojd75@mailru>
2024-07-14 22:11:11 +0300

    fixed3

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import base
+from catalog.views import base, products
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', base, name='base'),
+    path('catalog/<int:pk>/', products, name='products')
 ]
