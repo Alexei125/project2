98b6e37832ac3c031dcc15a192d3dc85896b6082
Alexei <rojd75@mailru>
2024-08-09 22:02:16 +0300

    users2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ProductListView, ProductDetailView
+from catalog.views import ProductListView, ProductDetailView, MyView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="base"),
     path("catalog/<int:pk>/", ProductDetailView.as_view(), name="products"),
+    path('catalog/create', MyView.as_view(), name='product_form')
 ]
