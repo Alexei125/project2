16a6b2798b9e26a0a902c08c3aa80858722382ec
Alexei <rojd75@mailru>
2024-08-18 19:32:37 +0300

    fixed_

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import User
+
 
 class Category(models.Model):
     name = models.CharField(
@@ -50,6 +52,20 @@ class Product(models.Model):
     update_at = models.DateField(
         verbose_name="Дата последнего изменения",
     )
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        verbose_name="Владелец",
+        related_name="products",
+        blank=True,
+        null=True,
+    )
+    is_published = models.BooleanField(
+        default=False, verbose_name="Опубликовано", help_text="Опубликовать продукт"
+    )
+
+    def __str__(self):
+        return f"{self.name}: {self.price_for_buy} руб."
 
     class Meta:
         verbose_name = "Продукт"
@@ -62,3 +78,41 @@ class Product(models.Model):
 
         def __str__(self):
             return self.name
+
+
+class Version(models.Model):
+    product = models.ForeignKey(
+        Product,
+        related_name="versions",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name="Продукт",
+    )
+    version_number = models.CharField(
+        max_length=10,
+        verbose_name="Номер версии",
+        help_text="Введите номер версии",
+        null=True,
+        blank=True,
+    )
+    version_name = models.CharField(
+        max_length=100,
+        verbose_name="Название версии",
+        help_text="Введите название версии",
+        null=True,
+        blank=True,
+    )
+    is_version_active = models.BooleanField(
+        default=False,
+        verbose_name="Активная версия",
+        help_text="Является ли версия активной",
+    )
+
+    class Meta:
+        verbose_name = "Версия"
+        verbose_name_plural = "Версии"
+        ordering = ["version_number", "version_name"]
+
+    def __str__(self):
+        return self.version_name
--- catalog/views.py
+++ catalog/views.py
@@ -1,22 +1,19 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import PermissionDenied
-from django.views.generic import ListView, DetailView
+from django.forms import inlineformset_factory
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
 
-from catalog.models import Product
+from catalog.forms import ProductForm, VersionForm, ProductModeratorForm
+from catalog.models import Product, Version
 
 
 class ProductListView(LoginRequiredMixin, ListView):
     model = Product
-    template_name = "catalog/category/html"
-    login_url = "/login/"
-    redirect_field_name = "redirect_to"
 
 
 class ProductDetailView(LoginRequiredMixin, DetailView):
     model = Product
-    template_name = "catalog/product_list.html"
-    login_url = "/login/"
-    redirect_field_name = "redirect_to"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -26,6 +23,43 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
         else:
             context['formset'] = ProductFormset(instance=self.object)
         return context
+
+    def form_valid(self, form):
+        formset = self.get_context_data()["formset"]
+        self.object = form.save()
+        if formset.is_valid():
+            formset.instance = self.object
+            formset.save()
+        return super().form_valid(form)
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:product_list")
+
+    def form_valid(self, form):
+        product = form.save(commit=False)
+        product.owner = self.request.user
+        product.save()
+        return redirect("catalog:product_detail", product.pk)
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("catalog:product_list")
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        ProductFormset = inlineformset_factory(Product, Version, VersionForm, extra=1)
+        if self.request.method == "POST":
+            context_data["formset"] = ProductFormset(
+                self.request.POST, instance=self.object
+            )
+        else:
+            context_data["formset"] = ProductFormset(instance=self.object)
+        return context_data
 
     def form_valid(self, form):
         context = self.get_context_data()
@@ -38,12 +72,17 @@ class ProductDetailView(LoginRequiredMixin, DetailView):
         else:
             return self.render_to_response(self.get_context_data(form=form, formset=formset))
 
+    def get_form_class(self):
 
-def get_form_class(self):
-    user = self.request.user
-    if user == self.object.owner:
-        return ProductForm
-    if user.has_perm(["can_delite_product", "can_change_category",
-                      "can_description_product"]):
-        return ProductModeratorForm
-    raise PermissionDenied
+        user = self.request.user
+        if user == self.object.owner:
+            return ProductForm
+        if user.has_perm(["can_delite_product", "can_change_category",
+                          "can_description_product"]):
+            return ProductModeratorForm
+        raise PermissionDenied
+
+
+class ProductDeleteView(LoginRequiredMixin, DeleteView):
+    model = Product
+    success_url = reverse_lazy("catalog:product_list")
